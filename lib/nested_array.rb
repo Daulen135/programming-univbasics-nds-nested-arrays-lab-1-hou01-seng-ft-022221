e9b5ed6b344db00ce35423bdc4f7991d7cbb6054
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-05 23:04:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -54,6 +54,8 @@ def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+  matrix_update=assembled_matrix
+  
 end
 
 describe 'When a learning to update cells in nested arrays,' do
