733606abeee928df4d07ec9e8cd0cdd0cefabf38
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-05 23:27:25 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -57,8 +57,8 @@ def matrix_update(matrix, row, column, new_value)
   # Return the updated matrix
   
 
-  matrix[row][column]="Lemons"
-  matrix[row][column]="Onions"
+  matrix[row][column]=new_value
+  
 
   matrix
 end
