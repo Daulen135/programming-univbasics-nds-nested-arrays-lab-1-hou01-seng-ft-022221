cfb5e37f72991b769d6b2597bfe5240c6f2e4f25
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-05 23:09:17 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -59,11 +59,4 @@ def matrix_update(matrix, row, column, new_value)
   matrix[row]column]=new_value
 end
 
-describe 'When a learning to update cells in nested arrays,' do
-  describe 'when given a matrix, row, column, and new_value argument,' do
-    it 'the matrix_update method returns an updated matrix' do
-      local_copy_of_matrix = assembled_matrix.dup
-      result = matrix_update(local_copy_of_matrix, 0, 2, "Lemons")
-      expect(result[0]).to eq(["Grapefruit", "Pineapple", "Lemons", "Watermelon", "Eggplant"])
-      result = matrix_update(result, 0, 2, "Onions")
-      expect(result[0]).to eq(["Grapefruit", "Pineapple", "Onions", "Watermelon", "Eggplant"])
+d
